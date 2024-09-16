acbe042beab4e160adfe4bbf651e45add7395624
Fany Louise <gandra.fany@gmail.com>
2024-09-16 17:20:10 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Navbar from './sections/Navbar'
+export const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto">
+      <Navbar></Navbar>
+    </main>
+  )
+}
+export default App
