6decf86174f299f68edfcbc874fd90760cf5598d
Fany Louise <gandra.fany@gmail.com>
2024-09-17 17:17:34 -0300

    navbar

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,17 +1,47 @@
 import { useState } from "react"
-/*1:39 */
+import { navLinks } from "../constants/index.js"
+/*1:47*/
+
+const NavItems = () => {
+  return (
+    <>
+    <ul className="nav-ul">
+      {navLinks.map(({id, name, href})=>(
+      <li key={id} className="nav-li">
+        <a href={href} className="text-neutral-400 hover:text-white transition-colors" onClick={()=>{}}>{name}</a>
+      </li>))}
+    </ul>
+  </>
+  )
+}
 const Navbar = () => {
-  const [isOpen, serIsOpen] = useState(false);
-  const toggleMenu = () => serIsOpen();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    console.log('Antes de clicar:', isOpen);
+    setIsOpen((prevIsOpen) => !prevIsOpen);
+    console.log('Depois de clicar:', isOpen);
+  };
+
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
       <div className="max-w-7xl mx-auto">
         <div className="flex justify-between items-center py-5 mx-auto c-space ">
           <a href="./" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">Logo</a>
-          <button onClick={toggleMenu}>
-            <img src={isOpen ? "./assets/close.svg":"./assets/menu.svg"} alt="toggle" className="w-6 h-6" />
+
+          <button onClick={toggleMenu} className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex" aria-label="Toggle menu">
+            <img src={isOpen ? "./assets/close.svg": "./assets/menu.svg"} alt="toggle" className="w-6 h-6" />
           </button>
+
+          <nav className="sm:flex hidden">
+            <NavItems />
+          </nav>
         </div>
+      </div>
+      <div className={`nav-siderbar ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
+        <nav className="p-5">
+          <NavItems />
+        </nav>
       </div>
     </header>
   )
