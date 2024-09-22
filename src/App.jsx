874982ec07449ab0b69975bf0893b7201e57b0c8
fanylouise <gandra.fany@gmail.com>
2024-09-22 19:47:38 -0300

    contacts, projects, footer working, starting experience

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
 import About from './sections/About'
 import Projects from './sections/Projects'
+import Clients from './sections/Clients'
+import Contacts from './sections/Contacts'
+import Footer from './sections/Footer'
 export const App = () => {
   return (
     <main className="max-w-7xl mx-auto">
@@ -9,6 +12,9 @@ export const App = () => {
       <Hero/>
       <About/>
       <Projects/>
+      <Clients/>
+      <Contacts/>
+      <Footer/>
     </main>
   )
 }
