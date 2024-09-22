a24fd0c68dcf9d04a42672f75779084e7bf4ef0d
fanylouise <gandra.fany@gmail.com>
2024-09-22 16:30:57 -0300

    About ok, starting projects section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
+import About from './sections/About'
+import Projects from './sections/Projects'
 export const App = () => {
   return (
     <main className="max-w-7xl mx-auto">
       <Navbar></Navbar>
       <Hero/>
+      <About/>
+      <Projects/>
     </main>
   )
 }
